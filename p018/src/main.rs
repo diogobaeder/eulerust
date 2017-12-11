a0142785a12978a6da5feb46cb1e7ece96fb2794
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2017-12-11 07:26:36 -0200

    Optimizing algorithm to prepare for Problem 067

--- p018/src/main.rs
+++ p018/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn read_triangle(path: &str) -> Vec<Vec<u8>> {
-    let mut triangle: Vec<Vec<u8>> = Vec::new();
+fn read_triangle(path: &str) -> Vec<Vec<usize>> {
+    let mut triangle: Vec<Vec<usize>> = Vec::new();
 
     let mut f = File::open(path).unwrap();
     let mut contents = String::new();
@@ -13,7 +13,7 @@ fn read_triangle(path: &str) -> Vec<Vec<u8>> {
         triangle.push(
             line.split_whitespace()
                 .map(|num_str| {
-                    num_str.trim_left_matches("0").parse::<u8>().unwrap()
+                    num_str.trim_left_matches("0").parse::<usize>().unwrap()
                 })
                 .collect(),
         );
@@ -22,19 +22,23 @@ fn read_triangle(path: &str) -> Vec<Vec<u8>> {
     triangle
 }
 
-fn node_sum(triangle: &[&[u8]], sum: usize, i: usize, j: usize) -> usize {
-    let sum = sum + (triangle[i][j] as usize);
-    if i == triangle.len() - 1 {
-        return sum;
+fn highest_sum(triangle: Vec<Vec<usize>>) -> usize {
+    let mut i = triangle.len() - 2;
+    let mut prev_row = triangle[i + 1].clone();
+
+    loop {
+        let row = triangle[i].clone();
+        prev_row = row.iter()
+            .enumerate()
+            .map(|(j, num)| num + prev_row[j].max(prev_row[j + 1]))
+            .collect();
+        if i == 0 {
+            break;
+        }
+        i -= 1;
     }
-    node_sum(triangle, sum, i + 1, j).max(node_sum(triangle, sum, i + 1, j + 1))
-}
 
-fn highest_sum(triangle: Vec<Vec<u8>>) -> usize {
-    let row_slices: Vec<&[u8]> = triangle.iter().map(|r| r.as_slice()).collect();
-    let triangle_slices: &[&[u8]] = row_slices.as_slice();
-
-    node_sum(triangle_slices, 0, 0, 0)
+    prev_row[0]
 }
 
 fn main() {
@@ -56,21 +60,21 @@ mod tests {
 
     #[test]
     fn highest_sum_1() {
-        let triangle: Vec<Vec<u8>> = vec![vec![1], vec![1, 1], vec![1, 1, 1]];
+        let triangle: Vec<Vec<usize>> = vec![vec![1], vec![1, 1], vec![1, 1, 1]];
 
         assert_eq!(highest_sum(triangle), 3);
     }
 
     #[test]
     fn highest_sum_2() {
-        let triangle: Vec<Vec<u8>> = vec![vec![2], vec![3, 1], vec![1, 4, 1]];
+        let triangle: Vec<Vec<usize>> = vec![vec![2], vec![3, 1], vec![1, 4, 1]];
 
         assert_eq!(highest_sum(triangle), 9);
     }
 
     #[test]
     fn highest_sum_3() {
-        let triangle: Vec<Vec<u8>> = vec![vec![3], vec![1, 4], vec![1, 1, 5]];
+        let triangle: Vec<Vec<usize>> = vec![vec![3], vec![1, 4], vec![1, 1, 5]];
 
         assert_eq!(highest_sum(triangle), 12);
     }
